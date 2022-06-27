d89da513940ae2fc36e613317b912b9d9587b0b1
wisberry <wisberry2022@gmail.com>
2022-06-27 11:55:11 +0900

    06/27 upload

---
+++ html_run/js/slick/slick_04.js
@@ -0,0 +1,12 @@
+$(function () {
+  $('.product_slider').slick({
+    slidesToShow: 5,
+    centerMode: true,
+    arrows: false,
+  });
+
+  $('.product_slider02').slick({
+    slidesToShow: 5,
+    centerMode: true,
+  });
+})
