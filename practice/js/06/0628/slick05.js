bba25173fa84dac97cd3ef2799badc0011719889
wisberry <wisberry2022@gmail.com>
2022-06-28 17:50:53 +0900

    06/28 upload

---
+++ practice/js/06/0628/slick05.js
@@ -0,0 +1,13 @@
+$(function () {
+  $('.main_slider01').slick({
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 1,
+  });
+
+  $('.main_slider02').slick({
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 3,
+  });
+})
