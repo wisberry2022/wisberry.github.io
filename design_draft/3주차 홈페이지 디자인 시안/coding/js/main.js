b7b35931f697cc85a89670d19ae9cff178f7fb2a
inseo <wisberry2022@gmail.com>
2022-07-04 21:23:08 +0900

    07/04 home upload

---
+++ design_draft/3주차 홈페이지 디자인 시안/coding/js/main.js
@@ -0,0 +1,12 @@
+$(function() {
+  $('#header .lang_switch span').on('click', function() {
+    $(this).addClass('lang_toggle');
+    $(this).siblings().removeClass('lang_toggle');
+  });
+
+  $('main .main_visual .container .main_slider').slick({
+    arrows: false,
+    centerMode: true,
+    centerPadding: '170px',
+  });
+})
